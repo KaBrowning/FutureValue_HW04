6f20726c2306d731d6f7f1031e4a4ce1eb920d71
Kathryn Browning <kbrowni3@my.westga.edu>
2015-01-17 10:07:54 -0500

    Created the page load event. Formatted the new product item in GetSelectedProduct according to ReSharper rules

--- source/Order.aspx.cs
+++ source/Order.aspx.cs
@@ -9,26 +9,36 @@ public partial class Order : System.Web.UI.Page
 
     protected void Page_Load(object sender, System.EventArgs e)
     {
-        
+        //bind drop-down list on first load
+        //get and show product on every load
+        if (!IsPostBack) this.ddlProducts.DataBind();
+        this.selectedProduct = this.GetSelectedProduct();
+        this.lblName.Text = this.selectedProduct.Name;
+        this.lblShortDescription.Text = this.selectedProduct.ShortDescription;
+        this.lblLongDescription.Text = this.selectedProduct.LongDescription;
+        this.lblUnitPrice.Text = this.selectedProduct.UnitPrice.ToString("c") + "each";
+        this.imgProduct.ImageUrl = "Images/Products/" + this.selectedProduct.ImageFile;
     }
 
     private Product GetSelectedProduct()
     {
         //get row from SqlDataSource based on value in dropdown list
-        DataView productsTable = (DataView)
+        var productsTable = (DataView)
             this.SqlDataSource.Select(DataSourceSelectArguments.Empty);
         productsTable.RowFilter = string.Format("ProductID = '{0}'",
             this.ddlProducts.SelectedValue);
         var row = (DataRowView) productsTable[0];
 
         //create a new product object and load with data from row
-        Product p = new Product();
-        p.ProductId = row["ProductID"].ToString();
-        p.Name = row["Name"].ToString();
-        p.ShortDescription = row["ShortDescription"].ToString();
-        p.LongDescription = row["LongDescription"].ToString();
-        p.UnitPrice = (decimal) row["UnitPrice"];
-        p.ImageFile = row["ImageFile"].ToString();
+        var p = new Product
+        {
+            ProductId = row["ProductID"].ToString(),
+            Name = row["Name"].ToString(),
+            ShortDescription = row["ShortDescription"].ToString(),
+            LongDescription = row["LongDescription"].ToString(),
+            UnitPrice = (decimal) row["UnitPrice"],
+            ImageFile = row["ImageFile"].ToString()
+        };
         return p;
     }
 }
