7e0d95cebeaa5fd36fd14484ba4b6131d2d8fe9f
Kathryn Browning <kbrowni3@my.westga.edu>
2015-01-17 10:01:44 -0500

    Added the GetSelectedProduct method. Keeping the inline comments until I get more comfortable with C#

--- source/Order.aspx.cs
+++ source/Order.aspx.cs
@@ -1,5 +1,34 @@
 ﻿
+using System.ComponentModel;
+using System.Data;
+using System.Web.UI;
+
 public partial class Order : System.Web.UI.Page
 {
-    
+    private Product selectedProduct;
+
+    protected void Page_Load(object sender, System.EventArgs e)
+    {
+        
+    }
+
+    private Product GetSelectedProduct()
+    {
+        //get row from SqlDataSource based on value in dropdown list
+        DataView productsTable = (DataView)
+            this.SqlDataSource.Select(DataSourceSelectArguments.Empty);
+        productsTable.RowFilter = string.Format("ProductID = '{0}'",
+            this.ddlProducts.SelectedValue);
+        var row = (DataRowView) productsTable[0];
+
+        //create a new product object and load with data from row
+        Product p = new Product();
+        p.ProductId = row["ProductID"].ToString();
+        p.Name = row["Name"].ToString();
+        p.ShortDescription = row["ShortDescription"].ToString();
+        p.LongDescription = row["LongDescription"].ToString();
+        p.UnitPrice = (decimal) row["UnitPrice"];
+        p.ImageFile = row["ImageFile"].ToString();
+        return p;
+    }
 }
