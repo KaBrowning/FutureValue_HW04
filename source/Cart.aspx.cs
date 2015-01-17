f4d33b017ad8985641c379a8d242952cef91c84c
Kathryn Browning <kbrowni3@my.westga.edu>
2015-01-17 14:29:17 -0500

    Got rid of some inline comments. Completed the assignment. Everything passes ReSharper and the application works as expected.

--- source/Cart.aspx.cs
+++ source/Cart.aspx.cs
@@ -15,10 +15,8 @@ public partial class Cart : System.Web.UI.Page
     /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
     protected void Page_Load(object sender, System.EventArgs e)
     {
-        //retrieve cart object from session state on every post back
         this._cart = CartItemList.GetCart();
 
-        //on initial page load, add cart items to list control
         if (!IsPostBack)
         {
             this.DisplayCart();
@@ -30,10 +28,8 @@ public partial class Cart : System.Web.UI.Page
     /// </summary>
     private void DisplayCart()
     {
-        //remove all current items from list control
         this.lstCart.Items.Clear();
 
-        //loop thorugh cart and add each item's Display value to the control
         for (var i = 0; i < this._cart.Count; i++)
         {
             this.lstCart.Items.Add(this._cart[i].Display());
@@ -49,20 +45,17 @@ public partial class Cart : System.Web.UI.Page
     /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
     protected void btnRemove_Click(object sender, System.EventArgs e)
     {
-        //if cart contains items and user has selected an item...
         if (this._cart.Count <= 0)
         {
             return;
         }
         if (this.lstCart.SelectedIndex > -1)
         {
-            //remove selected items from cart and re-add cart items
             this._cart.RemoveAt(this.lstCart.SelectedIndex);
             this.DisplayCart();
         }
         else
         {
-            //if no items is selected, notify user
             this.lblMessage.Text = "Please select an item to remove.";
         }
     }
@@ -75,7 +68,6 @@ public partial class Cart : System.Web.UI.Page
     /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
     protected void btnEmpty_Click(object sender, System.EventArgs e)
     {
-        //if cart has items, clear both cart and list control
         if (this._cart.Count <= 0)
         {
             return;
